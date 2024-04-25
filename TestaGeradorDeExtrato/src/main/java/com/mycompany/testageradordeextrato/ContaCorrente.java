671ab2cdbcb6fcdfb79244ac0012c3b48cffc2fd
diaraujo115 <160798100+diaraujo115@users.noreply.github.com>
2024-04-25 20:28:35 -0300

    Add files via upload

---
+++ TestaGeradorDeExtrato/src/main/java/com/mycompany/testageradordeextrato/ContaCorrente.java
@@ -0,0 +1,17 @@
+
+
+package com.mycompany.testageradordeextrato;
+
+public class ContaCorrente extends Conta{
+    private double limite;
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    
+    
+}
---
+++ TestaGeradorDeExtrato/src/main/java/com/mycompany/testageradordeextrato/ContaPoupanca.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.testageradordeextrato;
+
+public class ContaPoupanca extends Conta{
+    private int diaDoAniversario;
+
+    public int getDiaDoAniversario() {
+        return diaDoAniversario;
+    }
+
+    public void setDiaDoAniversario(int diaDoAniversario) {
+        this.diaDoAniversario = diaDoAniversario;
+    }
+    
+    
+    
+}
